f49281b399fd88c99ac614147f0b05f95d207a3b
장예은 <>
2023-11-14 14:47:21 +0900

    백 커밋

---
+++ src/main/java/com/esgproject/daaang_univ/DAaaNgUnivApplication.java
@@ -0,0 +1,15 @@
+package com.esgproject.daaang_univ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@SpringBootApplication
+public class DAaaNgUnivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DAaaNgUnivApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esgproject/daaang_univ/DAaaNgUnivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esgproject.daaang_univ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DAaaNgUnivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
